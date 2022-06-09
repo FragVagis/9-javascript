de07a61308c0d28d5e230da9f2e43ebd18c7fcfb
dmitrij <dzimaskurb@gmail.com>
2022-06-09 10:11:04 +0300

    first commit

---
+++ js/if.js
@@ -0,0 +1,11 @@
+const diena = 3;
+if (diena === 1) {
+    console.log('pirmadienis');
+} else {
+    if (diena === 2) {
+        console.log('antradienis');
+    } else {
+        console.log('treciadienis');
+    }
+}
+
