da18ebade24f0e5eb07a36ae80ed55d04ed0f5b0
dmitrij <dzimaskurb@gmail.com>
2022-06-20 11:10:57 +0300

    concat, set

---
+++ js/concat.js
@@ -0,0 +1,16 @@
+console.clear();
+const a = [1, 2, 3];
+const b = [4, 5, 6];
+const e = [7, 8, 9];
+
+console.log('a:', a);
+console.log('b:', b);
+
+console.log('-----------');
+const c = a.concat(e, b);
+const d = [...a, ...b, ...e];
+
+console.log('a:', a);
+console.log('b:', b);
+console.log('c:', c);
+console.log('d:', d);
